f36d4a159bd1a2a06e1f94076d219a0c3d109448
Pavel Kochkin <pavel.kochkin@inventry.co.uk>
2020-04-13 22:06:41 +0300

    Add project files.

---
+++ HhVacanciesParser/Startup.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HhVacanciesParser
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+
+            services.AddSingleton<VacanciesParser>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env, VacanciesParser vacanciesParser)
+        {
+            var salaryNodes = await vacanciesParser.GetSalaryNodes(AreasWithCodes.Minsk, "designer");
+
+            var usdSalaries = salaryNodes.Where(x => x.InnerText.Contains("USD"));
+            
+            var salaryValues = new List<int>();
+
+            foreach (var salary in usdSalaries)
+            {
+                string salaryString = salary.InnerText;
+
+                if (salaryString.StartsWith("от")) //от 500 USD
+                {
+                    //очистить стрингу оставив только цифры
+                    //спарсить ее в инт
+                    //добавить к этому значению 25% (т.к. зп от, значит минимальная, но тут хз)
+                    //добавить в salaryValues
+                }
+
+                if (salaryString.StartsWith("до")) //до 1000 USD
+                {
+                    //очистить стрингу оставив только цифры
+                    //спарсить ее в инт
+                    //отнять 25% (логика та же что и выше)
+                    //добавить в salaryValues
+                }
+
+                if (salaryString.Contains('-')) // 1000-2 000 USD 
+                {
+                    //переписать говнокод внизу
+                    //он берет два значения, парсит их и берет среднее
+                    var withoutUsd = salaryString.Remove(salary.InnerText.Length - 3, 3);
+                    var withoutSpaces = Regex.Replace(withoutUsd, @"\s+", "");
+                    string[] subs = withoutSpaces.Split('-');
+
+                    var x = int.Parse(subs[0]);
+                    var y = int.Parse(subs[1]);
+
+                    var result = ((y + x) / 2);
+                }
+                else
+                {
+                    //TODO: process salaries with just digit value, like 1000USD
+                }
+            }
+        }
+    }
+}
---
+++ HhVacanciesParser/VacanciesParser.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HtmlAgilityPack;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HhVacanciesParser
+{
+    public class VacanciesParser
+    {
+        private readonly HttpClient _httpClient;
+        private const string _hhSalaryNodeXpath = "//span[contains(@data-qa, 'vacancy-compensation')]";
+
+        readonly List<HtmlNode> salaryNodes = new List<HtmlNode>();
+        List<HtmlNode> returnedNodes = new List<HtmlNode>();
+
+
+        public VacanciesParser()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<List<HtmlNode>> GetSalaryNodes(int areaCode, string vacancyName)
+        {
+            int pageCount = 0;
+
+
+            while (returnedNodes != null)
+            {
+                var url = CombineUrl(areaCode, vacancyName, pageCount);
+                var response = await _httpClient.GetAsync(url);
+                var responseContentString = await response.Content.ReadAsStringAsync();
+
+                var htmlDoc = new HtmlDocument();
+
+                htmlDoc.LoadHtml(responseContentString);
+
+                returnedNodes = htmlDoc.DocumentNode.SelectNodes(_hhSalaryNodeXpath)?.ToList();
+
+                if (returnedNodes != null)
+                    salaryNodes.AddRange(returnedNodes);
+
+                pageCount++;
+            }
+
+            return salaryNodes;
+        }
+
+        private string CombineUrl(int areaCode, string vacancyName, int page = 0)
+        {
+            return $"https://hh.ru/search/vacancy?area={areaCode}&st=searchVacancy&text={vacancyName}&only_with_salary=true&page={page}";
+        }
+    }
+
+    public class AreasWithCodes
+    {
+        public static int Minsk = 1002;
+        public static int Belarus = 16;
+        public static int Moscow = 1;
+    }
+}
